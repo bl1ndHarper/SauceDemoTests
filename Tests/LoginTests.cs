2b3b3961bb2819848c29823b4df7d456d6ccb7a0
Yaroslav Postelnykov <yarikpost@gmail.com>
2025-06-25 15:20:25 +0300

    Logging expanded
    Logging detalized in LoginTests.cs

--- Tests/LoginTests.cs
+++ Tests/LoginTests.cs
@@ -26,12 +26,23 @@ namespace SauceDemoTests.Tests
         [InlineData("firefox")]
         public void UC1_LoginWithEmptyCredentials_ShouldShowUsernameRequired(string browser)
         {
+            _logger.Info($"[UC1] Starting test in browser: {browser}");
+
             InitDriver(browser);
+
+            _logger.Info("[UC1] Clearing Username and Password fields (leave empty)");
             _loginPage.Username.Clear();
             _loginPage.Password.Clear();
+
+            _logger.Info("[UC1] Clicking Login button");
             _loginPage.LoginButton.Click();
 
-            _loginPage.ErrorMessage.Text.Should().Contain("Username is required");
+            string actualMessage = _loginPage.ErrorMessage.Text;
+            _logger.Info($"[UC1] Actual error message: '{actualMessage}'");
+            _logger.Info("[UC1] Expecting error message to contain: 'Username is required'");
+
+            actualMessage.Should().Contain("Username is required");
+
             _logger.Info($"[UC1] Passed on {browser}");
         }
 
@@ -40,12 +51,25 @@ namespace SauceDemoTests.Tests
         [InlineData("firefox")]
         public void UC2_LoginWithUsernameOnly_ShouldShowPasswordRequired(string browser)
         {
+            _logger.Info($"[UC2] Starting test in browser: {browser}");
+
             InitDriver(browser);
+
+            _logger.Info("[UC2] Typing valid username");
             _loginPage.Username.SendKeys("standard_user");
+
+            _logger.Info("[UC2] Clearing Password field (leave empty)");
             _loginPage.Password.Clear();
+
+            _logger.Info("[UC2] Clicking Login button");
             _loginPage.LoginButton.Click();
 
-            _loginPage.ErrorMessage.Text.Should().Contain("Password is required");
+            string actualMessage = _loginPage.ErrorMessage.Text;
+            _logger.Info($"[UC2] Actual error message: '{actualMessage}'");
+            _logger.Info("[UC2] Expecting error message to contain: 'Password is required'");
+
+            actualMessage.Should().Contain("Password is required");
+
             _logger.Info($"[UC2] Passed on {browser}");
         }
 
@@ -54,23 +78,36 @@ namespace SauceDemoTests.Tests
         [InlineData("firefox")]
         public void UC3_ValidCredentials_ShouldLoginSuccessfully(string browser)
         {
+            _logger.Info($"[UC3] Starting test in browser: {browser}");
+
             InitDriver(browser);
+
+            _logger.Info("[UC3] Logging in with valid credentials");
             _loginPage.Login("standard_user", "secret_sauce");
 
+            _logger.Info("[UC3] Checking for 'Swag Labs' title on dashboard");
             var logo = _driver.FindElement(By.XPath("//div[@class='app_logo']"));
-            logo.Text.Should().Be("Swag Labs");
+            string actualText = logo.Text;
+
+            _logger.Info($"[UC3] Actual logo text: '{actualText}'");
+            _logger.Info("[UC3] Expecting: 'Swag Labs'");
+
+            actualText.Should().Be("Swag Labs");
+
             _logger.Info($"[UC3] Passed on {browser}");
         }
 
         private void InitDriver(string browser)
         {
             _driver = WebDriverFactory.GetDriver(browser);
+            _logger.Info($"[INIT] Navigating to https://www.saucedemo.com/");
             _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
             _loginPage = new LoginPage(_driver);
         }
 
         public void Dispose()
         {
+            _logger.Info("[CLEANUP] Quitting browser session");
             WebDriverFactory.QuitDriver();
         }
     }
